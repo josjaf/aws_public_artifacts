57b8146c29afb98e0f7200de872b39027ee5d379
Josh Jaffe <josjaffe@amazon.com>
2019-03-14 14:13:15 -0400

    adding assume role looping

--- run.py
+++ run.py
@@ -1,5 +1,53 @@
+import uuid
 import boto3
 
+def get_child_session(account_id, role_name, sts=None):
+    """
+    get session, with error handling, allows for passing in an sts client. This allows Account A > B > C where A cannot assume a role directly to C
+    :param account_id:
+    :param role_name:
+    :param sts:
+    :return:
+    """
+    # “/“ + name if not name.startswith(“/“) else name
+    try:
+        # allow for a to b to c if given sts client.
+        if sts == None:
+            client = boto3.client('sts')
+        else:
+            client = sts
+
+
+        response = client.get_caller_identity()
+        # remove the first slash
+        role_name = role_name[1:] if role_name.startswith("/") else role_name
+        # never have a slash in front of the role name
+        role_arn = 'arn:aws:iam::' + account_id + ':role/' + role_name
+        print("Creating new session with role: {} from {}".format(role_arn, response['Arn']))
+
+        response = client.assume_role(
+            RoleArn=role_arn,
+            RoleSessionName=str(uuid.uuid1())
+        )
+        credentials = response['Credentials']
+        session = boto3.Session(
+            aws_access_key_id=credentials['AccessKeyId'],
+            aws_secret_access_key=credentials['SecretAccessKey'],
+            aws_session_token=credentials['SessionToken']
+        )
+        return session
+    except botocore.exceptions.ClientError as e:
+
+        if e.response['Error']['Code'] == 'AccessDenied':
+            raise Exception(e)
+
+        elif 'Not authorized to perform sts:AssumeRole' in str(e):
+            raise Exception(f"ERROR:Not authorized to perform sts:AssumeRole on {role_arn}")
+        else:
+            raise Exception(e)
+
+    finally:
+        pass
 
 def get_org_accounts(session):
     org_client = session.client('organizations')
@@ -17,6 +65,11 @@ def main():
     session = boto3.session.Session()
     org_accounts = get_org_accounts(session)
     print(org_accounts)
+    for account in org_accounts:
+        child_session = get_child_session(account_id=account, role_name='OrganizationAccountAccessRole', sts=None)
+        ec2 = child_session.client('ec2')
+        response = ec2.describe_vpcs()
+        print(response)
     return
 
 if __name__ == '__main__':
